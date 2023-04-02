56854c29e5b7cb276240e00c64ac74a8c11ca284
Carlos Tavares <carlostavares2350@gmail.com>
2023-04-01 22:54:20 -0300

    Adicionando modelo fisico com arquivo DDL e DML com os dados dos livros, apenas

---
+++ dml-clubLivro.sql
@@ -0,0 +1,7 @@
+SET FOREIGN_KEY_CHECKS = 0; /*desativar a relação das tabelas, desativando as chaves estrangeiras - assim podemos adicionar os dados*/
+
+INSERT INTO LIVROS VALUES(
+1,"O Senhor dos Aneis","Rick Riordan",
+"Intríseca",
+"Aventura",
+100,55);
